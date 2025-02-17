6bdda58ef0f25d9b318621a9d5d6916ae0145482
ganjela <nikaganjelashvili@gmail.com>
2025-02-17 15:39:28 +0400

    change date format

--- macros/date_format.sql
+++ macros/date_format.sql
@@ -1,3 +1,3 @@
 {% macro format_date(column_name, granularity='hour') %}
-    date_trunc('{{ granularity }}', {{ column_name }})
+    date_trunc('{{ granularity }}', to_timestamp({{ column_name }}, 'YYYY-MM-DD"T"HH24:MI:SS.FF'))
 {% endmacro %}
